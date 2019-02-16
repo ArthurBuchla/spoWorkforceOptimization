0cc7c8cc958811704f4806e0fa40676831140d91
arthu <arthur.buchla@gmail.com>
2019-02-16 16:27:39 +0100

    Initial commit of spoWorkfoceOptimization coding challenge

---
+++ src/main/java/one/servicepartner/workforce/SpoWorkforceOptimizationApplication.java
@@ -0,0 +1,14 @@
+package one.servicepartner.workforce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpoWorkforceOptimizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpoWorkforceOptimizationApplication.class, args);
+	}
+
+}
+
